7d4584a22484b17aa639a17d180ec794809575d3
gkavin57 <gkavin77@gmail.com>
2022-11-18 14:15:35 +0530

    user component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let name = "kavin"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <User name="kavin" pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZev6gcBb-vziXchSyyXHoCeFrV5-yVC_ucyov8LsE&s" />
+      <User name="guru" pic="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg" />
+      <User name="kk" pic="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg" />
+
     </div>
   );
+}
+
+function User({name,pic}) {
+  
+  return (
+    <section>
+      <img src={pic} alt="ll" className="aa"></img>
+      <h1 className="abc"> hello, {name} ❤💖</h1>
+    </section > 
+    
+  )
 }
 
 export default App;
